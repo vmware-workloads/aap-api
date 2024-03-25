b285eda8c0f4ed41118972e9459094423e55e181
Charles Lee <cleeistaken@gmail.com>
2024-03-25 09:52:47 -0400

    create inventory and add hosts MVP

--- aap_invoke.py
+++ aap_invoke.py
@@ -1,106 +1,220 @@
 import requests
+import uuid
+import urllib3
 import urllib.parse
+
+from requests.auth import HTTPBasicAuth
+from typing import List, Union
+
+
+class AapHost(object):
+    def __init__(
+        self,
+        name: str,
+        variables: str = None,
+        instance_id: str = None,
+        description: str = None,
+        enabled: bool = None,
+    ):
+        self.name = name
+
+        if variables is not None:
+            self.variables = variables
+
+        if instance_id is not None:
+            self.instance_id = instance_id
+
+        if description is not None:
+            self.description = description
+
+        if enabled is not None:
+            self.enabled = enabled
+
+
+
+class AapApi(object):
+    URL_TOKEN = "api/v2/tokens/"
+    URL_INVENTORY = "api/v2/inventories/"
+    URL_ORGANIZATION = "api/v2/organizations/"
+    URL_HOSTS = "api/v2/hosts/"
+    URL_BULK_HOST_CREATE = "api/v2/bulk/host_create/"
+    DEFAULT_ORGANIZATION_ID = 1
+
+    def __init__(
+        self, base_url: str, username: str, password: str, ssl_verify: bool = True
+    ):
+
+        # Ansible Automation Platform details
+        self.base_url = base_url
+        self.username = username
+        self.password = password
+        self.ssl_verify = ssl_verify
+
+        # Basic Auth credentials
+        self._auth = HTTPBasicAuth(username=self.username, password=self.password)
+
+        # POST for OATH2 token
+        response = requests.post(
+            url=urllib.parse.urljoin(self.base_url, url=self.URL_TOKEN),
+            auth=self._auth,
+            verify=ssl_verify,
+        )
+
+        # Check for successful token creation
+        response.raise_for_status()
+
+        # Get the required data
+        data = response.json()
+        self._token = data.get("token")
+        self._token_id = data.get("id")
+
+    def __dict__(self) -> dict:
+        return {
+            "base_url": self.base_url,
+            "username": self.username,
+            "token": self._token,
+            "token_id": self._token_id,
+        }
+
+    def get_organization_id(self, name: str) -> int:
+        """Search for an organization and return its ID"""
+        url = urllib.parse.urljoin(
+            self.base_url, url=self.URL_ORGANIZATION + f"?search={name}"
+        )
+        headers = {
+            "Authorization": "Bearer " + self._token,
+        }
+        response = requests.get(url=url, headers=headers, verify=self.ssl_verify)
+        response.raise_for_status()
+
+        # Find the organization
+        # Note: We have to search because a search for 'foo' would return entries
+        #       that include 'foo', 'foobar', 'thatfoo'.
+        results = response.json().get("results", [])
+        matches = list(
+            filter(
+                lambda organization: organization.get("name") == name, results
+            )
+        )
+
+        # Return organization ID if found, otherwise default
+        if len(matches) < 1:
+            return self.DEFAULT_ORGANIZATION_ID
+        return matches[0].get("id")
+
+    def get_inventory_id(self, name: str) -> Union[None, int]:
+        """Search for an inventory and return its ID"""
+        url = urllib.parse.urljoin(
+            self.base_url, url=self.URL_INVENTORY + f"?search={name}"
+        )
+        headers = {
+            "Authorization": "Bearer " + self._token,
+        }
+        response = requests.get(url=url, headers=headers, verify=self.ssl_verify)
+        response.raise_for_status()
+
+        # Find the inventory
+        # Note: We have to search because a search for 'foo' would return entries
+        #       that include 'foo', 'foobar', 'thatfoo'.
+        # Get the required data
+        results = response.json().get("results", [])
+        matches = list(filter(lambda inventory: inventory.get("name") == name, results))
+
+        # Return inventory ID if found
+        if len(matches) < 1:
+            return None
+        return matches[0].get("id")
+
+    def create_inventory(
+        self, name: str, organization_id: int = DEFAULT_ORGANIZATION_ID
+    ):
+        """Create a new inventory and add hosts to the inventory"""
+
+        if self.get_inventory_id(name=name) is not None:
+            print(f"Inventory name { name } already exists.")
+            name = f"{ name }-{str(uuid.uuid1())}"
+            print(f"Generated unique inventory name {name}.")
+
+        url = urllib.parse.urljoin(self.base_url, url=self.URL_INVENTORY)
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self._token,
+        }
+        data = {
+            "name": name,
+            "description": "Created via Aria Automation API",
+            "organization": organization_id,
+        }
+        response = requests.post(
+            url=url, headers=headers, json=data, verify=self.ssl_verify
+        )
+        response.raise_for_status()
+        inventory_id = response.json().get("id")
+        return inventory_id
+
+    def add_hosts_to_inventory(self, inventory_id: int, hosts: List[AapHost]):
+        """Add hosts to am inventory
+        ref. https://www.redhat.com/en/blog/bulk-api-in-automation-controller
+        """
+        url = urllib.parse.urljoin(self.base_url, url=self.URL_BULK_HOST_CREATE)
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + self._token,
+        }
+        data = {
+            "inventory": inventory_id,
+            "hosts": [vars(host) for host in hosts],
+        }
+        response = requests.post(
+            url=url, headers=headers, json=data, verify=self.ssl_verify
+        )
+        response.raise_for_status()
 
 
 def handler(context, inputs):
-    class AapInvoke(object):
-        def __init__(self, base_url: str, username: str, password: str):
-
-            # Ansible Automation Platform details
-            self.base_url = base_url
-            self.username = username
-            self.password = password
-
-            # Headers for JSON content type
-            self._headers = {"Content-Type": "application/json"}
-
-            # Authentication token
-            self._auth_token = None
-
-            # Inventory
-            self._inventory_id = None
-
-        def __dict__(self) -> dict:
-            return {
-                "base_url": self.base_url,
-                "username": self.username,
-                "inventory_id": self._inventory_id,
-            }
-
-        def __repr__(self):
-            return f"{self.__class__.__name__}(base_url={self.base_url}, username={self.username}, inventory_id={self._inventory_id})"
-
-        def is_authenticated(self) -> bool:
-            return self._auth_token is not None and len(self._auth_token) > 0
-
-        def is_inventory_created(self) -> bool:
-            return self._inventory_id is not None
-
-        def authenticate(self):
-            """Authenticate with the AAP and return the auth token."""
-            auth_url = urllib.parse.urljoin(self.base_url, "api/v2/authtoken/")
-            response = requests.post(
-                auth_url,
-                headers=self._headers,
-                json={"username": self.username, "password": self.password},
-            )
-            response.raise_for_status()  # Raises an error if the authentication fails
-            self._auth_token = response.json()["token"]
-
-        def create_inventory(self, inventory_name: str, organization_id: int = 1):
-
-            if not self.is_authenticated():
-                raise RuntimeError(
-                    f"Not authenticated. Please authenticate before calling this method."
-                )
-
-            """Create a new inventory and return its ID."""
-            inventory_url = urllib.parse.urljoin(self.base_url, "api/v2/inventories/")
-            self._headers["Authorization"] = f"Token {self._auth_token}"
-            data = {
-                "name": inventory_name,
-                "description": "Created via Aria Automation API",
-                "organization": organization_id,
-            }
-            response = requests.post(inventory_url, headers=self._headers, json=data)
-            response.raise_for_status()
-            self._inventory_id = response.json()["id"]
-
-        def add_host_to_inventory(
-                self, inventory_id, host_name: str, host_description: str
-        ):
-
-            if not self.is_authenticated():
-                raise RuntimeError(
-                    f"Not authenticated. Please authenticate before calling this method."
-                )
-
-            if not self.is_inventory_created():
-                raise RuntimeError(
-                    f"Inventory not defined. Please create an inventory before calling this method."
-                )
-
-            """Add a host to the specified inventory."""
-            hosts_url = urllib.parse.urljoin(self.base_url, "api/v2/hosts/")
-
-            self._headers["Authorization"] = f"Token {self._auth_token}"
-            data = {
-                "name": host_name,
-                "description": host_description,
-                "inventory": inventory_id,
-            }
-            response = requests.post(hosts_url, headers=self._headers, json=data)
-            response.raise_for_status()
-            return response.json()
-
     base_url = inputs["base_url"]
     username = inputs["username"]
     password = context.getSecret(inputs["password"])
-    inventory_name = inputs["inventory_name"]
-    hosts = inputs["hosts"]
+    ssl_verify = inputs.get('ssl_verify', True)
+    inventory_name = inputs.get('inventory_name', 'aap-api-inventory')
+    hosts = [AapHost(**host) for host in inputs.get('hosts', [])]
 
-    aap = AapInvoke(base_url=base_url, username=username, password=password)
-    aap.authenticate()
-    aap.create_inventory(inventory_name=inventory_name)
+    if not ssl_verify:
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-    return f"Inventory created: {str(aap)}"
+    aap = AapApi(
+        base_url=base_url, username=username, password=password, ssl_verify=False
+    )
+    organization_id = aap.get_organization_id(name="Default")
+    inventory_id = aap.create_inventory(name=inventory_name, organization_id=organization_id)
+    aap.add_hosts_to_inventory(inventory_id=inventory_id, hosts=hosts)
+
+
+if __name__ == "__main__":
+    test = {
+        "base_url": "https://wdc-ansible.vcf01.isvlab.vmware.com",
+        "username": "admin",
+        "password": "P@ssword123!",
+        "ssl_verify": False,
+        "inventory_name": "foobar",
+        "hosts": [
+            {"name": "foo1"},
+            {"name": "foo2"},
+            {"name": "foo3"}
+        ],
+    }
+
+    class Passthrough(object):
+        def __init__(self):
+            pass
+
+        @classmethod
+        def getSecret(cls, x):
+            return x
+
+    handler(Passthrough(), test)
+
+
+
+
+
