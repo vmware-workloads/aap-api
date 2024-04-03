c13bea054b60fea6781173668217e0d0ade62de5
Dharmesh Bhatt <dharmesh.bhatt@broadcom.com>
2024-04-03 11:11:57 +0000

    Update aap_delete.py

--- aap_delete.py
+++ aap_delete.py
@@ -44,21 +44,6 @@ class AapHost(object):
         self.variables = json.dumps(variables)
         self.groups = host_groups
 
-    @classmethod
-    def create_app_hosts(cls, hosts: list, host_variables: dict, host_groups: dict) -> List['AapHost']:
-        aap_hosts = []
-        for host_list in hosts:
-            for host in host_list:
-                variables = host_variables.get(host.get("name"), {})
-                groups = host_groups.get(host.get('resourceName'), [])
-                aap_hosts.append(AapHost(host=host, host_variables=variables, host_groups=groups))
-        return aap_hosts
-
-    def get_host_data(self):
-        return {
-            "name": self.name,
-            "variables": self.variables,
-        }
 
 
 class AapApi(object):
@@ -137,17 +122,15 @@ class AapApi(object):
 
     def __delete(self, path: str, id: int):
         url = urllib.parse.urljoin(self.base_url, url=path)
-        delete_url = urllib.parse.urljoin(self.url, url=id)
+        delete_url = urllib.parse.urljoin(url, url=str(id))
         headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + self._token,
         }
-        response = requests.delete(
-            url=delete_url, headers=headers, verify=self.ssl_verify
-        )
+        response = requests.delete(url=delete_url, headers=headers, verify=self.ssl_verify)
         response.raise_for_status()
-        if response.status_code not in [204]:
-            return response.json()            
+        if response.status_code not in [202]:
+            return "RuntimeError in delete:" + str(response.status_code)
 
     @classmethod
     def __find(cls, name: str, results: list) -> list:
@@ -206,134 +189,18 @@ class AapApi(object):
             "id": matches[0].get("id")
         }
 
-    def find_group_by_name(self, name: str, inventory_id: int) -> Union[None, dict]:
-        """Search for a group and return its ID"""
-        path = self.PATH_GROUPS + f"?inventory={inventory_id}"
-        response = self.__get(path=path)
-        results = response.get("results", [])
 
-        # Search for exact name matches
-        # Note: The previous query returns all the groups part of an inventory,
-        #       so we need to filter for the exact name
-        matches = self.__find(name=name, results=results)
-
-        # We should only have one match
-        if len(matches) > 1:
-            raise RuntimeError(
-                f"Found {len(matches)} groups with name {name} in inventory {inventory_id}."
-            )
-
-        # Return 'None' if none found
-        if len(matches) < 1:
-            return None
-
-        # Return the group id
-        return {
-            "name": name,
-            "id": matches[0].get("id")
-        }
-
-    def find_job_template_by_name(self, name: str) -> Union[None, dict]:
-        """Search for a job template by name."""
-        path = self.PATH_JOB_TEMPLATES + f"?search={name}"
-        response = self.__get(path=path)
-        results = response.get("results", [])
-
-        # Search for exact name matches
-        # Note: The previous query returns all the groups part of an inventory,
-        #       so we need to filter for the exact name
-        matches = self.__find(name=name, results=results)
-
-        # We should only have one match
-        if len(matches) > 1:
-            raise RuntimeError(f"Found {len(matches)} templates with name {name}.")
-
-        # Return 'None' if none found
-        if len(matches) < 1:
-            return None
-
-        # Return the inventory id
-        return {
-            "name": name,
-            "id": matches[0].get("id")
-        }
-
-    def create_inventory(
+    def lookup_inventory(
         self, name: str, organization_id: int = DEFAULT_ORGANIZATION_ID
     ) -> dict:
         """Create a new inventory and add hosts to the inventory"""
         inventory = self.find_inventory_by_name(name=name)
-        if inventory is None:
-            data = {
-                "name": name,
-                "description": "Created via Aria Automation API",
-                "organization": organization_id,
-            }
-            response = self.__post(path=self.PATH_INVENTORY, data=data)
-            inventory = {
-                "name": name,
-                "id": response.get("id")
-            }
         return inventory
 
-    def delete_inventory(self, name: str, inventory_id: int)  
+    def delete_inventory(self, inventory_id: int):  
         """Delete an inventory"""
         response = self.__delete(path=self.PATH_INVENTORY, id=inventory_id)
 
-
-    def create_group(self, name: str, inventory_id: int, description: str = None) -> dict:
-        """Create a new group"""
-        group = self.find_group_by_name(name=name, inventory_id=inventory_id)
-        if group is None:
-            data = {
-                "name": name,
-                "description": "" if description is None else description,
-                "inventory": inventory_id,
-            }
-            response = self.__post(path=self.PATH_GROUPS, data=data)
-            group = {
-                "name": name,
-                "id": response.get("id")
-            }
-        return group
-
-    def add_hosts_to_inventory(self, inventory_id: int, hosts: List[AapHost]) -> List[AapHost]:
-        """Add hosts to am inventory
-        ref. https://www.redhat.com/en/blog/bulk-api-in-automation-controller
-        """
-        data = {
-            "inventory": inventory_id,
-            "hosts": [host.get_host_data() for host in hosts],
-        }
-        response = self.__post(path=self.PATH_BULK_HOST_CREATE, data=data)
-        for host in hosts:
-            host.id = next(item.get("id") for item in response.get("hosts") if item["name"] == host.name)
-        return hosts
-
-    def add_hosts_to_groups(self, aap_hosts: List[AapHost], aap_groups: List[dict]) -> None:
-        """Add hosts to the specified groups."""
-        groups_ids = {}
-        for aap_group in aap_groups:
-            groups_ids[aap_group.get('name')] = aap_group.get('id')
-
-        for aap_host in aap_hosts:
-            for group_name in aap_host.groups:
-                group_id = groups_ids[group_name]
-                group_path = self.PATH_GROUP_ADD.substitute({"group_id": group_id})
-                data = {
-                    "id": aap_host.id
-                }
-                self.__post(path=group_path, data=data)
-
-    def launch_job_template(self, job_template_id: dict, inventory_id: int) -> dict:
-        """Launch a job template with a specific inventory."""
-        path = self.PATH_JOB_TEMPLATES_LAUNCH.substitute({"job_template_id": job_template_id})
-        data = {
-            "inventory": inventory_id,
-        }
-        response = self.__post(path=path, data=data)
-        pass
-        return response
 
     def get_job_status(self, job: dict):
         """Retrieve the status of a specific job."""
@@ -350,6 +217,9 @@ class AapApi(object):
             else:
                 print(f"Job {job.get('id')} is {status}. Waiting {interval} seconds out of {max_timeout_seconds}.")
                 time.sleep(interval)
+                
+    def clean(self):
+        response = self.__delete(path=self.PATH_TOKEN, id=self._token_id)
 
 
 def handler(context, inputs):
@@ -364,25 +234,13 @@ def handler(context, inputs):
     organization_name = inputs.get("organization_name", "Default")
     inventory_name = inputs.get("inventory_name", "aap-api")
     
-    #hosts = inputs.get("hosts", [])
-    #host_variables = inputs.get("host_variables", {})
-    #groups = [group_name for group_name in inputs.get("host_groups", {}).keys()]
-    #host_groups = invert_dict(d=inputs.get("host_groups", {}), name="resourceName")
-    #job_template_name = inputs.get("job_template_name")
-
-    # Create AAP Hosts
-    #aap_hosts = AapHost.create_app_hosts(hosts=hosts, host_variables=host_variables, host_groups=host_groups)
 
     if verbose:
         print(f"base_url: '{base_url}'")
         print(f"username: '{username}'")
         print(f"ssl_verify: '{ssl_verify}'")
         print(f"inventory_name: '{inventory_name}'")
-        print(f"host_variables: '{host_variables}'")
-        print(f"groups: '{groups}'")
-        print(f"host_groups: '{host_groups}'")
-        print(f"hosts: '{[vars(aap_host) for aap_host in aap_hosts]}'")
-        print(f"job_template_name: '{job_template_name}")
+
 
     # Disable SSL warning if we are not verifying the ansible host SSL certificate
     if not ssl_verify:
@@ -397,47 +255,17 @@ def handler(context, inputs):
     # Find the organization id
     aap_organization = aap.find_organization_by_name(name=organization_name)
 
-    # Find the job template
-    #aap_job_template = aap.find_job_template_by_name(name=job_template_name)
-    #if aap_job_template is None:
-    #    raise ValueError(f"Could not find a job template with name '{job_template_name}'")
 
     # Get the inventory id
     # If an inventory with that exact name exists, we return its id.
     # If an inventory with that exact name does not exist, we create one and return the id.
-    aap_inventory = aap.create_inventory(name=inventory_name, organization_id=aap_organization.get("id"))
+    aap_inventory = aap.lookup_inventory(name=inventory_name, organization_id=aap_organization.get("id"))
 
     # Delete the inventory
-    aap_deleted = aap.delete_inventory(id = aap_inventory)
-
-    # Get the group ids
-    # If a group with the exact name exists, we return its id.
-    # If a group with the exact name does not exist, we create one and return the id.
-    #aap_groups = [aap.create_group(name=group, inventory_id=aap_inventory.get("id")) for group in groups]
-
-    # Add the hosts to the inventory
-    # The host names *must* be unique within the inventory.
-    #aap_hosts = aap.add_hosts_to_inventory(inventory_id=aap_inventory.get("id"), hosts=aap_hosts)
-
-    # Add the hosts to inventory groups
-    #aap.add_hosts_to_groups(aap_hosts=aap_hosts, aap_groups=aap_groups)
-
-    # Start the job template with the created inventory
-    #aap_job = aap.launch_job_template(job_template_id=aap_job_template.get("id"), inventory_id=aap_inventory.get("id"))
-
-    # Wait for the job to complete
-    #aap.wait_for_job_completion(job=aap_job)
+    aap_inventory_deleted = aap.delete_inventory(inventory_id=aap_inventory.get("id"))
+    
+    # Cleanup: delete the access token_id
+    aap_cleanup = aap.clean()
 
 
-if __name__ == "__main__":
-    test = json.load(open("test-02.json"))
 
-    class Passthrough(object):
-        def __init__(self):
-            pass
-
-        @classmethod
-        def getSecret(cls, x):
-            return x
-
-    handler(Passthrough(), test)
